a3e971eafa63a13172156396bd7574823f33ace2
Wellington <wellingtomdev@gmail.com>
2024-05-08 17:08:36 -0300

    add connector

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const createNotifier = require('./modules/createNotifier')
 const sortVersions = require('./modules/sortVersions')
 const getObjectId = require('./modules/getObjectId')
 const createQueue = require('./modules/createQueue')
+const connector = require('./modules/connector')
 const random = require('./modules/random')
 const delay = require('./modules/delay')
 const json = require('./modules/json')
@@ -13,6 +14,7 @@ module.exports = {
     sortVersions,
     getObjectId,
     createQueue,
+    connector,
     random,
     delay,
     json
